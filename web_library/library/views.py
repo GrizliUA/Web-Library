1100db140b922d008fbf58016b8b0887f744d178
Struk Maksym <maksum07770@gmail.com>
2022-06-17 20:13:22 +0300

    permissions + deleted trash

--- web_library/library/views.py
+++ web_library/library/views.py
@@ -1,16 +1,9 @@
-from unicodedata import category
-from django.forms import model_to_dict
-from rest_framework import generics, viewsets
-from django.shortcuts import render
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import Anime
+from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly 
 from .serializers import AnimeSerializer
-
-
 
 
 class AnimeAPIList(generics.ListCreateAPIView):
@@ -21,106 +14,9 @@ class AnimeAPIList(generics.ListCreateAPIView):
 class AnimeAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = Anime.objects.all()
     serializer_class = AnimeSerializer
+    permission_classes = (IsOwnerOrReadOnly, )
 
 class AnimeAPIDestroy(generics.RetrieveDestroyAPIView):
     queryset = Anime.objects.all()
     serializer_class = AnimeSerializer
-    permission_classes = (IsAdminUser, )
-
-
-
-
-
-
-
-
-
-
-
-#class AnimeViewSet(viewsets.ReadOnlyModelViewSet):
-#    #queryset = Anime.objects.all()
-#    serializer_class = AnimeSerializer
-#
-#    def get_queryset(self):
-#        pk = self.kwargs.get("pk")
-#        if not pk:
-#            return Anime.objects.all()[:3]
-#        return Anime.objects.filter(pk=pk)
-#
-#    @action(methods=['get'], detail=True)
-#    def genres(self, request, pk = None):
-#        anime = Anime.objects.get(pk=pk)
-#        return Response({'genres': anime.genre})
-
-
-
-
-
-
-
-
-
-
-
-# class AnimeAPIList(generics.ListCreateAPIView): #GET/POST
-#     queryset = Anime.objects.all()
-#     serializer_class = AnimeSerializer
-
-
-
-# class AnimeAPIUpdate(generics.UpdateAPIView):   #PUT
-#     queryset = Anime.objects.all()
-#     serializer_class = AnimeSerializer    
-
-
-
-
-# class AnimeAPIDetailView(generics.RetrieveUpdateDestroyAPIView):    #CRUD
-#     queryset = Anime.objects.all()
-#     serializer_class = AnimeSerializer  
-
-
-
-#class AnimeAPIView(APIView):
-    #def get(self, request):
-        #anime = Anime.objects.all()
-        #return Response({'posts': AnimeSerializer(anime,many=True).data})
-
-    #def post(self,request):
-        #serializer = AnimeSerializer(data=request.data)
-        #serializer.is_valid(raise_exception=True)
-        #serializer.save()
-        
-        #return Response({'post': serializer.data})
-
-    #def put(self, request, *args, **kwargs):
-        #pk = kwargs.get("pk",None)
-        #if not pk:
-            #return Response({"error": "Method PUT not allowed"})
-        
-        #try:
-            #instance = Anime.objects.get(pk=pk)
-        #except:
-            #return Response({"error": "Object PUT not exist"})
-
-        #serializer = AnimeSerializer(data=request.data, instance=instance)
-        #serializer.is_valid(raise_exception=True)
-        #serializer.save()
-        #return Response({"post": serializer.data})
-
-    #def delete(self, request, *args, **kwargs):
-        #pk = kwargs.get("pk",None)
-        #if not pk:
-            #return Response({"error": "Method DELETE not allowed"})
-
-        #try:
-            #instance = Anime.objects.delete(pk=pk)
-        #except:
-            #return Response({"error": "Object DELETE not exist"})
-
-        #serializer = AnimeSerializer(data=request.data, instance=instance)
-        #serializer.is_valid(raise_exception=True)
-        #serializer.save()
-
-
-        #return Response({"post": "delete post" + str(pk)})        
+    permission_classes = (IsAdminOrReadOnly, )
--- web_library/web_library/urls.py
+++ web_library/web_library/urls.py
@@ -18,33 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 from library.views import *
-#from rest_framework import routers
-
-
-
-
-# class MyCustomRouter(routers.SimpleRouter):
-#     routes = [
-#         routers.Route(url=r'^{prefix}$',
-#                       mapping={'get': 'list'},
-#                       name='{basename}-list',
-#                       detail=False,
-#                       initkwargs={'suffix': 'List'}),
-#         routers.Route(url=r'^{prefix}/{lookup}$',
-#                       mapping={'get': 'retrieve'},
-#                       name='{basename}-detail',
-#                       detail=True,
-#                       initkwargs={'suffix': 'Detail'})
-#     ]
-
-#router = MyCustomRouter()
-#router.register(r'anime', AnimeViewSet,basename='anime')
-
-
-
-
-
-
 
 
 
@@ -53,9 +26,4 @@ urlpatterns = [
     path('api/v1/anime/', AnimeAPIList.as_view()),
     path('api/v1/anime/<int:pk>/', AnimeAPIUpdate.as_view()),
     path('api/v1/animedelete/<int:pk>/', AnimeAPIDestroy.as_view())
-    #path('api/v1/', include(router.urls)),
-    #path('api/v1/animelist/', AnimeViewSet.as_view({'get': 'list'})),
-    #path('api/v1/animelist/<int:pk>/', AnimeViewSet.as_view({'put': 'update'})),
-    #path('api/v1/animedetail/<int:pk>/', AnimeAPIDetailView.as_view())
-
 ]
