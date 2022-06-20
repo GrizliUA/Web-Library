7401aebd9fecef9ec4b82ca60b6de29907fc974f
Struk Maksym <maksum07770@gmail.com>
2022-06-20 03:34:31 +0300

    Update 1(20.06.2022)

--- web_library/library/urls.py
+++ web_library/library/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('anime/', AnimeAPIList.as_view()),
-    path('anime/<int:pk>/', AnimeAPIUpdate.as_view()),
-    path('animedelete/<int:pk>/', AnimeAPIDestroy.as_view()),
+    path('anime/mine/', MyAnime.as_view()),
+    path('anime/<int:pk>/', AnimeByID.as_view()),
+    path('anime/genre/<slug:slug>/', AnimeByGenre.as_view()),
+    path('anime/user/<int:pk>/', AnimeByUserID.as_view()),
+    path('anime/', AnimeList.as_view()),
 ]
--- web_library/library/views.py
+++ web_library/library/views.py
@@ -1,22 +1,43 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-
+from rest_framework.permissions import IsAuthenticated
 from .models import Anime
-from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly 
+from .permissions import ReadOnly, IsOwnerOrAdminOrReadOnly
 from .serializers import AnimeSerializer
 
 
-class AnimeAPIList(generics.ListCreateAPIView):
-    queryset = Anime.objects.all()
+class MyAnime(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = AnimeSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly, )
 
-class AnimeAPIUpdate(generics.RetrieveUpdateAPIView):
-    queryset = Anime.objects.all()
-    serializer_class = AnimeSerializer
-    permission_classes = (IsOwnerOrReadOnly, )
+    def get_queryset(self):
+        user = self.request.user
+        return Anime.objects.filter(user=user)
 
-class AnimeAPIDestroy(generics.RetrieveDestroyAPIView):
-    queryset = Anime.objects.all()
+class AnimeByID(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrAdminOrReadOnly]
     serializer_class = AnimeSerializer
-    permission_classes = (IsAdminOrReadOnly, )
+    
+    def get_queryset(self, **kwargs):
+         pk = self.kwargs.get('pk')
+         return Anime.objects.filter(id=pk)
+
+class AnimeByGenre(generics.ListAPIView):
+    permission_classes = [ReadOnly]
+    serializer_class = AnimeSerializer
+
+    def get_queryset(self, **kwargs):
+        slug = self.kwargs.get('slug')
+        return Anime.objects.filter(genre=slug)
+
+class AnimeByUserID(generics.ListAPIView):
+    permission_classes = [ReadOnly]
+    serializer_class = AnimeSerializer
+
+    def get_queryset(self, **kwargs):
+        uid = self.kwargs.get('pk')
+        return Anime.objects.filter(user=uid)
+
+class AnimeList(generics.ListAPIView):
+    permission_classes = [ReadOnly]
+    serializer_class = AnimeSerializer
+    queryset = Anime.objects.all()
